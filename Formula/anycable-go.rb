0e47da4bbd6cda9b0b79100f51272612adaf7752
Vladimir Dementyev <dementiev.vm@gmail.com>
2017-12-22 12:36:55 +0300

    Fix link

--- Formula/anycable-go.rb
+++ Formula/anycable-go.rb
@@ -1,8 +1,8 @@
 class AnycableGo < Formula
   desc "Anycable Go WebSocket Server"
   homepage "https://github.com/anycable/anycable-go"
-  url "https://github.com/anycable/anycable-go/archive/0.5.2.tar.gz"
-  sha256 "4db6981fe7855d853d76c5854af0f7728d09ebde8a4344a6ab872ad914ca9bfc"
+  url "https://github.com/anycable/anycable-go/archive/v0.5.2.tar.gz"
+  sha256 "414f0a891d38dd1a739653695fbbb50db6a98e920b73dbe3b7a8a88cd06bd0e0"
 
   head "https://github.com/anycable/anycable-go.git"
 
