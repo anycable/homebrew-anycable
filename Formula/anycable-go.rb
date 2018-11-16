4fb8c6923cd4320cedd906538d542818c04aa88b
Sergey Ponomarev <me@sergey-ponomarev.ru>
2018-11-16 13:43:01 +0300

    Upgrade to 0.6.0

--- Formula/anycable-go.rb
+++ Formula/anycable-go.rb
@@ -1,20 +1,18 @@
 class AnycableGo < Formula
   desc "Anycable Go WebSocket Server"
   homepage "https://github.com/anycable/anycable-go"
-  url "https://github.com/anycable/anycable-go/archive/v0.5.4.tar.gz"
-  sha256 "19ca04182782af17e43bf08fb79a2e53bb1fc4e2340e5d3fa14c6ad620ed51eb"
-
-  head "https://github.com/anycable/anycable-go.git"
+  url "https://github.com/anycable/anycable-go/archive/v0.6.0.tar.gz"
+  sha256 "207734027f0a7ce4a9bfa25c30ae9576efcec1020feb15637c50f990dc18cedd"
 
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/anycable/anycable-go/").install Dir["*"]
-    system "go", "build", "-ldflags", "-X main.version=#{version}", "-o", "#{bin}/anycable-go", "-v", "github.com/anycable/anycable-go/"
+    system "go", "build", "-ldflags", "-s -w -X main.version=#{version}", "-o", "#{bin}/anycable-go", "-v", "github.com/anycable/anycable-go/cmd/anycable-go"
   end
 
   test do
-    assert_match /#{version}/, shell_output("#{bin}/anycable-go -version")
+    assert_match /#{version}/, shell_output("#{bin}/anycable-go -v")
   end
 end
